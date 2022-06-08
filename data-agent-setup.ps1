fa4d58e971b57cb68cda9fadd362634bfaa4941c
Gabriel Perroni <gperroni@commonwealth.com>
2022-06-08 10:15:41 -0300

    script for configuring ADO Data agents

---
+++ data-agent-setup.ps1
@@ -0,0 +1,25 @@
+##Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#PowerShell Core
+choco install powershell-core -y
+
+#Build Tools and SQL Package
+choco install visualstudio2022buildtools  -y
+choco install visualstudio2019sql --pre
+choco install ssdt17 -y
+
+#Profisee CLU
+Set-Location $HOME\Downloads
+Invoke-WebRequest -OutFile MasterDataMaestroDesktop_x64.msi -Uri https://mdmdev.commonwealth.com/profisee/api/MasterDataMaestroDesktop_x64.msi
+msiexec.exe /I $HOME\Downloads\MasterDataMaestroDesktop_x64.msi
+
+
+## Download ADO agent
+Invoke-WebRequest -OutFile vsts-agent-win-x64-2.204.0.zip -Uri https://vstsagentpackage.azureedge.net/agent/2.204.0/vsts-agent-win-x64-2.204.0.zip
+mkdir agent ; cd agent
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-2.204.0.zip", "$PWD")
+
+.\config.cmd
+
