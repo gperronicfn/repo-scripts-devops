6700400f94bf3bd71452f7c31ee389c02441a88e
Gabriel Perroni <gperroni@commonwealth.com>
2022-06-29 15:45:09 -0300

    added sahrepoint config file

---
+++ sharepoint-agent-setup.ps1
@@ -0,0 +1,26 @@
+##Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#PowerShell Core
+choco install powershell-core -y
+
+#PowerShell Az Module
+Install-Module -Name Az -Scope AllUsers -Repository PSGallery -Force
+
+#Build Tools and SQL Package
+choco install visualstudio2022buildtools  -y
+choco install ssdt17 -y
+
+#VS Test Build Tools
+choco install visualstudio2019testagent
+
+
+## Download ADO agent
+Set-Location $HOME\Downloads
+Invoke-WebRequest -OutFile vsts-agent-win-x64-2.204.0.zip -Uri https://vstsagentpackage.azureedge.net/agent/2.204.0/vsts-agent-win-x64-2.204.0.zip
+Set-Location C:\
+mkdir agent ; cd agent
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-2.204.0.zip", "$PWD")
+
+.\config.cmd
